7f4eb7f18b1370f853bafa56ec0f9533cdd9cf18
Hehe99hu <godmaher25@gmail.com>
2026-06-23 19:01:17 +0000

    Add mud dimension mod

---
+++ src/main/java/com/muddimension/dimension/MudDimensionMod.java
@@ -0,0 +1,11 @@
+package com.muddimension.dimension;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MudDimensionMod implements ModInitializer {
+    public static final String MOD_ID = "muddimension";
+
+    @Override
+    public void onInitialize() {
+    }
+}
